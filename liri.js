c84a89df69618e93bd163597530c639eeb61d702
jadibrahim99 <jadintx@gmail.com>
2020-04-04 18:34:10 -0500

    Variable set up with all of rthe correct node modules.

--- liri.js
+++ liri.js
@@ -1,4 +1,15 @@
 require("dotenv").config();
-
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+// Spotify API that pulls from keys.js
+//var Spotify = require("node-spotify-api");
+//var spotify = new Spotify(keys.spotify);
+// Acessing moment through Node
+var moment = require("moment");
+// This will use Axios to pull info from OMDB and BandsInTown API
+var axios = require("axios");
+// This will read the random.txt file for do-what-it-says function
+var fs = require("fs");
+var search = process.argv[2];
+var term = process.argv.slice(3).join(" ");
+
+
